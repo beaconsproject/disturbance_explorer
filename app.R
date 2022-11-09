f00b7abe1f29038323d15fd6e1fc277f6d316e23
Marc <medwards219@gmail.com>
2022-11-09 12:49:14 -0800

    Stopped map redrawing when button pressed. #3

--- app.R
+++ app.R
@@ -277,13 +277,13 @@ server = function(input, output) {
     })
 
     # Intactness
-    v <- eventReactive(input$goButton, {
+    intactness_sf <- eventReactive(input$goButton, {
         ifl <- st_difference(bnd(), footprint_sf())
     })
 
     # Intactness using minimum patch size (not yet implemented)
     ifl_min <- reactive({
-        x <- mutate(v(), area_km2=st_area(v())) #%>%
+        x <- mutate(intactness_sf(), area_km2=st_area(intactness_sf())) #%>%
         y <- filter(x, as.numeric(area_km2) > as.numeric(input$area1)*1000000)
     })
 
@@ -292,7 +292,10 @@ server = function(input, output) {
     ####################################################################################################
 
     # Map viewer
+    # Render the initial map
     output$map <- renderLeaflet({
+        
+        # Re-project
         bnd <- st_transform(bnd(), 4326)
         ifl2000 <- st_transform(ifl2000(), 4326)
         ifl2020 <- st_transform(ifl2020(), 4326)
@@ -300,78 +303,85 @@ server = function(input, output) {
         quartz <- st_transform(quartz(), 4326)
         areal <- st_transform(areal(), 4326)
         linear <- st_transform(linear(), 4326)
-        pal <- colorBin("YlOrRd", domain = fires$FIRE_YEAR, bins = c(1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020, Inf))
+        
+        #pal <- colorBin("YlOrRd", domain = fires$FIRE_YEAR, bins = c(1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020, Inf))
         labels <- sprintf("Fire year: %s<br/>Fire cause: %s", fires$FIRE_YEAR, fires$GENERAL_FIRE_CAUSE) %>% lapply(htmltools::HTML)
-        m <- leaflet(bnd) %>% 
-    		addProviderTiles("Esri.NatGeoWorldMap", group="Esri.NatGeoWorldMap") %>%
-	    	addProviderTiles("Esri.WorldImagery", group="Esri.WorldImagery") %>%
-            addPolygons(data=bnd, color='black', fill=F, weight=2, group="FDA") %>%
-            #addPolygons(data=fires, fillColor = ~pal(FIRE_YEAR), color='grey', weight=1, group="Fires", opacity=1, fillOpacity = 0.7,
-            addPolygons(data=fires, fillColor="red", color='grey', weight=1, group="Fires", opacity=1, fillOpacity=0.5,
-              highlightOptions = highlightOptions(weight=2, color="black", bringToFront=T),
-              label = labels,
-              labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px"), textsize = "15px", direction = "auto")) %>%
-            #addLegend(pal = pal, values = ~fires$FIRE_YEAR, opacity = 0.7, title = NULL, position = "bottomright") %>%
-            addPolygons(data=quartz, color='yellow', fill=F, weight=1, group="Quartz") %>%
-            addPolylines(data=linear, color='red', weight=1, group="Linear features", popup = ~paste("Industry: ", TYPE_INDUSTRY, "<br>", "Disturbance: ", TYPE_DISTURBANCE)) %>%
-            addPolygons(data=areal, color='black', fill=T, stroke=F, group="Areal features", popup = ~paste("Industry: ", TYPE_INDUSTRY, "<br>", "Disturbance: ", TYPE_DISTURBANCE), fillOpacity=input$alpha) %>%
-            addPolygons(data=ifl2020, color='darkgreen', fillOpacity=0.5, group="IFL 2020") %>%
-            addPolygons(data=ifl2000, color='darkgreen', fillOpacity=0.5, group="IFL 2000")
-            if (input$goButton) {
-                v <- st_transform(v(), 4326)
-                vv <- st_transform(footprint_sf(), 4326)
-                ifl_min <- st_transform(ifl_min(), 4326)
-                m <- m %>% addPolygons(data=v, color='blue', stroke=F, fillOpacity=input$alpha, group='Intactness') %>%
-                    addPolygons(data=vv, color='black', stroke=F, fillOpacity=input$alpha, group='Footprint') %>%
-                    addPolygons(data=ifl_min, color='darkgreen', stroke=F, fillOpacity=input$alpha, group='Intact min')
-            }
-        #pal <- colorBin("PuOr", fires$GENERAL_FIRE_CAUSE, bins = c(0, .1, .4, .9, 1))
-        m <- m %>% addLayersControl(position = "topleft",
-                baseGroups=c("Esri.NatGeoWorldMap", "Esri.WorldImagery"),
-                overlayGroups = c("IFL 2020","IFL 2000","Intactness","Footprint","Intact min","Fires","Quartz","Areal features","Linear features"),
-            options = layersControlOptions(collapsed = TRUE)) %>%
-                hideGroup(c("IFL 2020","IFL 2000","Intactness","Footprint","Intact min","Fires","Quartz","Areal features","Linear features"))
+        
+        leaflet() %>% 
+          
+          addProviderTiles("Esri.NatGeoWorldMap", group="Esri.NatGeoWorldMap") %>%
+          addProviderTiles("Esri.WorldImagery", group="Esri.WorldImagery") %>%
+          
+          addPolygons(data=bnd, color='black', fill=F, weight=2, group="FDA") %>%
+          #addPolygons(data=fires, fillColor = ~pal(FIRE_YEAR), color='grey', weight=1, group="Fires", opacity=1, fillOpacity = 0.7,
+          addPolygons(data=fires, fillColor="red", color='grey', weight=1, group="Fires", opacity=1, fillOpacity=0.5,
+            highlightOptions = highlightOptions(weight=2, color="black", bringToFront=T),
+            label = labels,
+            labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px"), textsize = "15px", direction = "auto")) %>%
+          #addLegend(pal = pal, values = ~fires$FIRE_YEAR, opacity = 0.7, title = NULL, position = "bottomright") %>%
+          addPolygons(data=quartz, color='yellow', fill=F, weight=1, group="Quartz") %>%
+          addPolylines(data=linear, color='red', weight=1, group="Linear features", popup = ~paste("Industry: ", TYPE_INDUSTRY, "<br>", "Disturbance: ", TYPE_DISTURBANCE)) %>%
+          addPolygons(data=areal, color='black', fill=T, stroke=F, group="Areal features", popup = ~paste("Industry: ", TYPE_INDUSTRY, "<br>", "Disturbance: ", TYPE_DISTURBANCE), fillOpacity=input$alpha) %>%
+          addPolygons(data=ifl2020, color='darkgreen', fillOpacity=0.5, group="IFL 2020") %>%
+          addPolygons(data=ifl2000, color='darkgreen', fillOpacity=0.5, group="IFL 2000") %>%
+          
+          #pal <- colorBin("PuOr", fires$GENERAL_FIRE_CAUSE, bins = c(0, .1, .4, .9, 1))
+          addLayersControl(position = "topleft",
+                           baseGroups=c("Esri.NatGeoWorldMap", "Esri.WorldImagery"),
+                           overlayGroups = c("IFL 2020","IFL 2000","Fires","Quartz","Areal features","Linear features"),
+                           options = layersControlOptions(collapsed = TRUE)) %>%
+          hideGroup(c("IFL 2020","IFL 2000","Fires","Quartz","Areal features","Linear features"))
         #m <- m %>% addLegend(pal=pal, values=~fires$GENERAL_FIRE_CAUSE, position=c("bottomright"), title="Fire cause", opacity=0.8)
-        m
+    })
+    
+    # Add/update intactness, footrpint and min layers when button clicked
+    observeEvent(input$goButton, {
+      
+      v <- st_transform(intactness_sf(), 4326)
+      vv <- st_transform(footprint_sf(), 4326)
+      ifl_min <- st_transform(ifl_min(), 4326)
+      
+      leafletProxy("map") %>%
+        clearGroup("Intactness") %>%
+        clearGroup("Footprint") %>%
+        clearGroup("Intact min") %>%
+        hideGroup(c("Footprint","Intactness","Intact min")) %>%
+        addPolygons(data=v, color='blue', stroke=F, fillOpacity=input$alpha, group='Intactness') %>%
+        addPolygons(data=vv, color='black', stroke=F, fillOpacity=input$alpha, group='Footprint') %>%
+        addPolygons(data=ifl_min, color='darkgreen', stroke=F, fillOpacity=input$alpha, group='Intact min') %>%
+        addLayersControl(position = "topleft",
+                         baseGroups=c("Esri.NatGeoWorldMap", "Esri.WorldImagery"),
+                         overlayGroups = c("IFL 2020","IFL 2000","Fires","Quartz","Areal features","Linear features","Footprint","Intactness","Intact min"),
+                         options = layersControlOptions(collapsed = TRUE))
     })
 
     # Intactness table
-    dta1 <- reactive({
-        x <- tibble(Map=c("FDA (km2)","IFL 2000 (%)","IFL 2020 (%)","Intactness (%)","Footprint (%)"), Area=NA)
-        x$Area[x$Map=="FDA (km2)"] <- round(st_area(bnd())/1000000,0)
-        x$Area[x$Map=="IFL 2000 (%)"] <- round(sum(st_area(ifl2000()))/st_area(bnd())*100,1)
-        x$Area[x$Map=="IFL 2020 (%)"] <- round(sum(st_area(ifl2020()))/st_area(bnd())*100,1)
-        if (input$goButton) {
-            x$Area[x$Map=="Intactness (%)"] <- round(sum(st_area(v()))/st_area(bnd())*100,1)
-            x$Area[x$Map=="Footprint (%)"] <- round(sum(st_area(footprint_sf()))/st_area(bnd())*100,1)
-        }
-        x
-    })
-
-	output$tab1 <- renderTable({
-        dta1()
-    })
+  	output$tab1 <- renderTable({
+  	  x <- tibble(Map=c("FDA (km2)","IFL 2000 (%)","IFL 2020 (%)","Intactness (%)","Footprint (%)"), Area=NA)
+  	  x$Area[x$Map=="FDA (km2)"] <- round(st_area(bnd())/1000000,0)
+  	  x$Area[x$Map=="IFL 2000 (%)"] <- round(sum(st_area(ifl2000()))/st_area(bnd())*100,1)
+  	  x$Area[x$Map=="IFL 2020 (%)"] <- round(sum(st_area(ifl2020()))/st_area(bnd())*100,1)
+  	  
+  	  # If button has been pressed at least once, keep the intactness/footprint values updated
+  	  if(input$goButton > 0) {
+  	    x$Area[x$Map=="Intactness (%)"] <- round(sum(st_area(intactness_sf()))/st_area(bnd())*100,1)
+  	    x$Area[x$Map=="Footprint (%)"] <- round(sum(st_area(footprint_sf()))/st_area(bnd())*100,1)
+  	  }
+  	  x
+  	})
 
     # Linear disturbances table
-    dta2 <- reactive({
-        km <- group_by(linear(), TYPE_DISTURBANCE) %>%
-            summarize(Length_km = sum(Length_km))
-        x <- tibble(Disturbance_type=km$TYPE_DISTURBANCE, Length_km=km$Length_km, Length_pct=Length_km/sum(Length_km)*100)
-    })
-
-	output$tab2 <- renderTable({
-        dta2()
-    })
-
+  	output$tab2 <- renderTable({
+  	  km <- group_by(linear(), TYPE_DISTURBANCE) %>%
+  	    summarize(Length_km = sum(Length_km))
+  	  x <- tibble(Disturbance_type=km$TYPE_DISTURBANCE, Length_km=km$Length_km, Length_pct=Length_km/sum(Length_km)*100)
+      })
+  
     # Areal disturbances table
-    dta3 <- reactive({
-        ha <- group_by(areal(), TYPE_DISTURBANCE) %>%
-            summarize(Area_ha = sum(Area_ha)/100)
-        x <- tibble(Disturbance_type=ha$TYPE_DISTURBANCE, Area_ha=ha$Area_ha, Area_pct=Area_ha/sum(Area_ha)*100)
-    })
-
-	output$tab3 <- renderTable({
-        dta3()
+  	output$tab3 <- renderTable({
+  	  ha <- group_by(areal(), TYPE_DISTURBANCE) %>%
+  	    summarize(Area_ha = sum(Area_ha)/100)
+  	  x <- tibble(Disturbance_type=ha$TYPE_DISTURBANCE, Area_ha=ha$Area_ha, Area_pct=Area_ha/sum(Area_ha)*100)
     })
 
     ####################################################################################################
@@ -465,7 +475,7 @@ server = function(input, output) {
         m <- m %>% addPolylines(data=linear, color='red', weight=1, group="Linear features") %>%
             addPolygons(data=areal, color='black', fill=T, stroke=F, group="Areal features", fillOpacity=input$alpha)
         if (input$goButton) {
-            v <- st_transform(v(), 4326)
+            v <- st_transform(intactness_sf(), 4326)
             vv <- st_transform(footprint_sf(), 4326)
             m <- m %>% addPolygons(data=v, color='blue', stroke=F, fillOpacity=input$alpha, group='Intactness') %>%
               addPolygons(data=vv, color='black', stroke=F, fillOpacity=input$alpha, group='Footprint')
@@ -496,7 +506,7 @@ server = function(input, output) {
             addPolylines(data=linear, color='red', weight=1, group="Linear features") %>%
             addPolygons(data=areal, color='black', fill=T, stroke=F, group="Areal features", fillOpacity=input$alpha)
             if (input$goButton) {
-                v <- st_transform(v(), 4326)
+                v <- st_transform(intactness_sf(), 4326)
                 vv <- st_transform(footprint_sf(), 4326)
                 m <- m %>% addPolygons(data=v, color='blue', stroke=F, fillOpacity=input$alpha, group='Intactness') %>%
                     addPolygons(data=vv, color='black', stroke=F, fillOpacity=input$alpha, group='Footprint')
@@ -511,8 +521,8 @@ server = function(input, output) {
     dta5 <- reactive({
         if (input$goButton) {
             x <- tibble(Class=c('Streams','LakesRivers'), Length_km=c(0,NA), Area_km2=c(NA,0), Intact_pct=0)
-            streams_intact <- st_intersection(streams(), v())
-            lakesrivers_intact <- st_intersection(lakesrivers(), v())
+            streams_intact <- st_intersection(streams(), intactness_sf())
+            lakesrivers_intact <- st_intersection(lakesrivers(), intactness_sf())
             x$Length_km[x$Class=='Streams'] <- round(sum(st_length(streams()))/1000,2)
             x$Area_km2[x$Class=='LakesRivers'] <- round(sum(st_area(lakesrivers()))/1000000,2)
             x$Intact_pct[x$Class=='Streams'] <- round(sum(st_length(streams_intact))/sum(st_length(streams()))*100,2)
@@ -535,7 +545,7 @@ server = function(input, output) {
 
     output$downloadMap <- downloadHandler(
         filename = function() {'fda_intactness.gpkg'},
-        content = function(file) {st_write(v(), dsn=file, layer=paste0('linear',input$buffer1,'_areal',input$buffer2,'.gpkg'))}
+        content = function(file) {st_write(intactness_sf(), dsn=file, layer=paste0('linear',input$buffer1,'_areal',input$buffer2,'.gpkg'))}
     )
 
 }
