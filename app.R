35e2a55fb60a784682a25f326773c2d0c6d08a86
Pierre R Vernier <pierre.vernier@gmail.com>
2023-11-07 10:08:14 +0100

    Update app.R

--- app.R
+++ app.R
@@ -8,10 +8,10 @@ library(markdown)
 library(shinyMatrix)
 
 fda_list <- c("fda10ab","fda10ad")
-x1 <- st_read("www/fda10ab.gpkg", 'sd_line', quiet=T) %>%
-  pull(TYPE_DISTURBANCE) %>% unique()
-x2 <- st_read("www/fda10ab.gpkg", 'sd_poly', quiet=T) %>% 
-  pull(TYPE_DISTURBANCE) %>% unique()
+x1_line <- st_read("www/fda10ab.gpkg", 'sd_line', quiet=T)
+x1 <- pull(x1_line, TYPE_DISTURBANCE) %>% unique()
+x2_poly <- st_read("www/fda10ab.gpkg", 'sd_poly', quiet=T) 
+x2 <- pull(x2_poly, TYPE_DISTURBANCE) %>% unique()
 m1 <- as.matrix(read_csv('docs/cas.csv')[40:60,2:4] %>% filter(TYPE_DISTURBANCE %in% x1))
 m2 <- as.matrix(read_csv('docs/cas.csv')[1:39,2:4] %>% filter(TYPE_DISTURBANCE %in% x2))
 
@@ -32,6 +32,8 @@ ui = dashboardPage(skin="blue",
         ),
          conditionalPanel(
            condition = "input.tabs == 'buffer'",
+           sliderInput("buffer1", label="Linear buffer size (m):", min=0, max=2000, value = 500, step=50, ticks=FALSE),
+           sliderInput("buffer2", label="Areal buffer size (m):", min=0, max=2000, value = 500, step=50, ticks=FALSE),
            sliderInput("area1", label="Min intact patch size (km2):", min=0, max=2000, value = 0, step=50, ticks=FALSE),
            hr(),
            actionButton("goButton", "Generate intactness map")
@@ -48,14 +50,18 @@ ui = dashboardPage(skin="blue",
       tabItem(tabName="overview",
             fluidRow(
                 #box(title = "Mapview", leafletOutput("map1", height=750) %>% withSpinner(), width=12),
-                tabBox(id = "one", width="12",
+                tabBox(id = "one", width="8",
                     tabPanel("Mapview", leafletOutput("map1", height=750) %>% withSpinner()),
-                    tabPanel("Buffer sizes", tags$h4("Define linear buffer sizes:"), matrixInput("linear_buffers", value=m1, rows=list(names=FALSE, extend=TRUE), cols=list(names=TRUE)), tags$h4("Define areal buffer sizes:"), matrixInput("areal_buffers", value=m2, rows=list(names=FALSE, extend=TRUE), cols=list(names=TRUE))),
-                    tabPanel("Statistics", tableOutput("tab1")),
+                    tabPanel("Custom buffers", checkboxInput("custom_buffers", "Use custom buffers", value=FALSE), tags$h4("Define linear buffer sizes:"), matrixInput("linear_buffers", value=m1, rows=list(names=FALSE, extend=TRUE), cols=list(names=TRUE)), tags$h4("Define areal buffer sizes:"), matrixInput("areal_buffers", value=m2, rows=list(names=FALSE, extend=TRUE), cols=list(names=TRUE))),
+                    #tabPanel("Detailed statistics", tableOutput("tab2")),
                     tabPanel("Overview", includeMarkdown("docs/overview.md")),
                     #tabPanel("Workflow", includeMarkdown("docs/workflow.md")),
                     tabPanel("Quick start", includeMarkdown("docs/quick_start.md")),
                     tabPanel("Datasets", includeMarkdown("docs/datasets.md"))
+                ),
+                tabBox(
+                    id = "two", width="4",
+                    tabPanel("Statistics", tableOutput("tab1"))
                 )
             )
         )
@@ -187,13 +193,35 @@ server = function(input, output, session) {
 
   observeEvent(c(input$select_fda,input$upload_poly), {
     x1 <- line() %>% pull(TYPE_DISTURBANCE) %>% unique()
-    m1 <- as.matrix(read_csv('docs/cas.csv')[40:60,2:4] %>% filter(TYPE_DISTURBANCE %in% x1))
+
+    y1 <- line() %>%
+      mutate(length_km=st_length(line())) %>%
+      st_drop_geometry() %>%
+      group_by(TYPE_DISTURBANCE) %>%
+      summarize(LENGTH_KM = round(as.numeric(sum(length_km)/1000),1))
+    cas <- read_csv('docs/cas.csv')[40:60,2:4] %>% filter(TYPE_DISTURBANCE %in% x1)
+    y <- left_join(cas, y1) %>%
+      relocate(TYPE_INDUSTRY, TYPE_DISTURBANCE, LENGTH_KM, BUFFER_SIZE)
+    m1 <- as.matrix(y)
+    
+    #m1 <- as.matrix(read_csv('docs/cas.csv')[40:60,2:4] %>% filter(TYPE_DISTURBANCE %in% x1))
     updateMatrixInput(session, 'linear_buffers', m1)
   })
 
   observeEvent(c(input$select_fda,input$upload_poly), {
     x2 <- poly() %>% pull(TYPE_DISTURBANCE) %>% unique()
-    m2 <- as.matrix(read_csv('docs/cas.csv')[1:39,2:4] %>% filter(TYPE_DISTURBANCE %in% x2))
+
+    y2 <- poly() %>%
+      mutate(area_km2=st_area(poly())) %>%
+      st_drop_geometry() %>%
+      group_by(TYPE_DISTURBANCE) %>%
+      summarize(AREA_KM2 = round(as.numeric(sum(area_km2)/1000000),3))
+    cas <- read_csv('docs/cas.csv')[1:39,2:4] %>% filter(TYPE_DISTURBANCE %in% x2)
+    y <- left_join(cas, y2) %>%
+      relocate(TYPE_INDUSTRY, TYPE_DISTURBANCE, AREA_KM2, BUFFER_SIZE)
+    m2 <- as.matrix(y)
+
+    #m2 <- as.matrix(read_csv('docs/cas.csv')[1:39,2:4] %>% filter(TYPE_DISTURBANCE %in% x2))
     updateMatrixInput(session, 'areal_buffers', m2)
   })
 
@@ -203,14 +231,19 @@ server = function(input, output, session) {
   ##############################################################################
   footprint_sf <- eventReactive(input$goButton, {
     aoi <- fda()
-    m1sub <- as_tibble(input$linear_buffers) %>% select(TYPE_DISTURBANCE, BUFFER_SIZE) %>%      
-      mutate(BUFFER_SIZE=as.integer(BUFFER_SIZE))
-    line <- left_join(line(), m1sub) %>% filter(!is.na(BUFFER_SIZE))
-    v1 <- st_union(st_buffer(line, line$BUFFER_SIZE))
-    m2sub <- as_tibble(input$areal_buffers) %>% select(TYPE_DISTURBANCE, BUFFER_SIZE) %>% 
-      mutate(BUFFER_SIZE=as.integer(BUFFER_SIZE))
-    poly <- left_join(poly(), m2sub) %>% filter(!is.na(BUFFER_SIZE))
-    v2 <- st_union(st_buffer(poly, poly$BUFFER_SIZE))
+    if (input$custom_buffers==TRUE) {
+      m1sub <- as_tibble(input$linear_buffers) %>% select(TYPE_DISTURBANCE, BUFFER_SIZE) %>%      
+        mutate(BUFFER_SIZE=as.integer(BUFFER_SIZE))
+      line <- left_join(line(), m1sub) %>% filter(!is.na(BUFFER_SIZE))
+      v1 <- st_union(st_buffer(line, line$BUFFER_SIZE))
+      m2sub <- as_tibble(input$areal_buffers) %>% select(TYPE_DISTURBANCE, BUFFER_SIZE) %>% 
+        mutate(BUFFER_SIZE=as.integer(BUFFER_SIZE))
+      poly <- left_join(poly(), m2sub) %>% filter(!is.na(BUFFER_SIZE))
+      v2 <- st_union(st_buffer(poly, poly$BUFFER_SIZE))
+    } else {
+      v1 <- st_union(st_buffer(line(), input$buffer1))
+      v2 <- st_union(st_buffer(poly(), input$buffer2))
+    }
     v <- st_intersection(st_union(v1, v2), st_buffer(aoi, 100))
   })
 
@@ -279,41 +312,63 @@ server = function(input, output, session) {
   })
 
   ##############################################################################
+  # Generate detailed statistics table
+  ##############################################################################
+    output$tab2 <- renderTable({
+      x <- line() %>%
+        mutate(length_km=st_length(line())) %>%
+        st_drop_geometry() %>%
+        group_by(TYPE_DISTURBANCE) %>%
+        summarize(LENGTH_KM = mean(length_km))
+      y <- left_join(as_tibble(m1), x) %>%
+        relocate(TYPE_INDUSTRY, TYPE_DISTURBANCE, LENGTH_KM, BUFFER_SIZE)
+      y
+    })
+
+  ##############################################################################
   # Generate statistics table
   ##############################################################################
     output$tab1 <- renderTable({
-      x <- tibble(Map=c("Area of interest (km2)", "Fires (%)", "IFL 2000 (%)", "IFL 2020 (%)", "Intactness (%)","Footprint (%)","Footprint + Fires (%)"), Area=NA)
+      x <- tibble(Attribute=c("Area of interest (km2)", "Linear disturbances (km)", "Areal disturbances (km2)", "Fires (%)", "IFL 2000 (%)", "IFL 2020 (%)", "Intactness (%)","Footprint (%)","Footprint + Fires (%)"), Value=NA)
       aoi <- sum(st_area(fda()))
-      x$Area[x$Map=="Area of interest (km2)"] <- round(aoi/1000000, 0)
-      x$Area[x$Map=="IFL 2000 (%)"] <- round(sum(st_area(ifl2000()))/sum(st_area(fda()))*100,1)
-      x$Area[x$Map=="IFL 2020 (%)"] <- round(sum(st_area(ifl2020()))/sum(st_area(fda()))*100,1)
+      x$Value[x$Attribute=="Area of interest (km2)"] <- round(aoi/1000000, 0)
+      x$Value[x$Attribute=="IFL 2000 (%)"] <- round(sum(st_area(ifl2000()))/sum(st_area(fda()))*100,1)
+      x$Value[x$Attribute=="IFL 2020 (%)"] <- round(sum(st_area(ifl2020()))/sum(st_area(fda()))*100,1)
 
       # If button has been pressed at least once, keep the intactness/footprint values updated
       if (is.null(input$upload_poly)) {
           aoi <- sum(st_area(fda()))
+          line <- st_intersection(st_geometry(line()), st_geometry(fda()))
+          x$Value[x$Attribute=="Linear disturbances (km)"] <- round(sum(st_length(line()))/1000,1)
+          poly <- st_intersection(st_geometry(poly()), st_geometry(fda()))
+          x$Value[x$Attribute=="Areal disturbances (km2)"] <- round(sum(st_area(poly()))/1000000,1)
           fires <- st_intersection(st_geometry(fires()), st_geometry(fda()))
-          x$Area[x$Map=="Fires (%)"] <- round(sum(st_area(fires()))/sum(st_area(fda()))*100,1)
+          x$Value[x$Attribute=="Fires (%)"] <- round(sum(st_area(fires()))/sum(st_area(fda()))*100,1)
           ifl2000 <- st_intersection(st_geometry(ifl2000()), st_geometry(fda()))
           ifl2020 <- st_intersection(st_geometry(ifl2020()), st_geometry(fda()))
-          x$Area[x$Map=="Area of interest (km2)"] <- round(st_area(fda())/1000000,0)
-          x$Area[x$Map=="IFL 2000 (%)"] <- round(sum(st_area(ifl2000))/sum(st_area(fda()))*100,1)
-          x$Area[x$Map=="IFL 2020 (%)"] <- round(sum(st_area(ifl2020))/sum(st_area(fda()))*100,1)
+          x$Value[x$Attribute=="Area of interest (km2)"] <- round(st_area(fda())/1000000,0)
+          x$Value[x$Attribute=="IFL 2000 (%)"] <- round(sum(st_area(ifl2000))/sum(st_area(fda()))*100,1)
+          x$Value[x$Attribute=="IFL 2020 (%)"] <- round(sum(st_area(ifl2020))/sum(st_area(fda()))*100,1)
         #}
       } else {
           aoi <- sum(st_area(aoi_bnd()))
+          line <- st_intersection(st_geometry(line()), st_geometry(aoi_bnd()))
+          x$Value[x$Attribute=="Linear disturbances (km)"] <- round(sum(st_length(line()))/1000,1)
+          poly <- st_intersection(st_geometry(poly()), st_geometry(aoi_bnd()))
+          x$Value[x$Attribute=="Areal disturbances (km2)"] <- round(sum(st_area(poly()))/1000000,1)
           fires <- st_intersection(st_geometry(fires()), st_geometry(aoi_bnd()))
-          x$Area[x$Map=="Fires (%)"] <- round(sum(st_area(fires()))/sum(st_area(aoi_bnd()))*100,1)
+          x$Value[x$Attribute=="Fires (%)"] <- round(sum(st_area(fires()))/sum(st_area(aoi_bnd()))*100,1)
           ifl2000 <- st_intersection(st_geometry(ifl2000()), st_geometry(aoi_bnd()))
           ifl2020 <- st_intersection(st_geometry(ifl2020()), st_geometry(aoi_bnd()))
-          x$Area[x$Map=="Area of interest (km2)"] <- round(st_area(aoi_bnd())/1000000,0)
-          x$Area[x$Map=="IFL 2000 (%)"] <- round(sum(st_area(ifl2000))/sum(st_area(aoi_bnd()))*100,1)
-          x$Area[x$Map=="IFL 2020 (%)"] <- round(sum(st_area(ifl2020))/sum(st_area(aoi_bnd()))*100,1)
+          x$Value[x$Attribute=="Area of interest (km2)"] <- round(st_area(aoi_bnd())/1000000,0)
+          x$Value[x$Attribute=="IFL 2000 (%)"] <- round(sum(st_area(ifl2000))/sum(st_area(aoi_bnd()))*100,1)
+          x$Value[x$Attribute=="IFL 2020 (%)"] <- round(sum(st_area(ifl2020))/sum(st_area(aoi_bnd()))*100,1)
       }
       if(input$goButton > 0) {
-        x$Area[x$Map=="Intactness (%)"] <- round(sum(st_area(intactness_sf()))/aoi*100, 1)
-        x$Area[x$Map=="Footprint (%)"] <- round(sum(st_area(footprint_sf()))/aoi*100, 1)
+        x$Value[x$Attribute=="Intactness (%)"] <- round(sum(st_area(intactness_sf()))/aoi*100, 1)
+        x$Value[x$Attribute=="Footprint (%)"] <- round(sum(st_area(footprint_sf()))/aoi*100, 1)
         foot_fires <- st_union(st_union(footprint_sf()), st_union(fires()))
-        x$Area[x$Map=="Footprint + Fires (%)"] <- round(sum(st_area(foot_fires))/aoi*100, 1)
+        x$Value[x$Attribute=="Footprint + Fires (%)"] <- round(sum(st_area(foot_fires))/aoi*100, 1)
       }
       x
     })
@@ -346,9 +401,9 @@ server = function(input, output, session) {
     }
   )
 
-  session$onSessionEnded(function() {
-    stopApp()
-  })
+  #session$onSessionEnded(function() {
+  #  stopApp()
+  #})
 
 }
 shinyApp(ui, server)
