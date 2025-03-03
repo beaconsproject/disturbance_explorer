fd038d3c2154d4f2757ea07f6584c0f3b91c9ceb
Pierre R Vernier <pierre.vernier@gmail.com>
2025-03-03 12:35:15 -0800

    Increased memory size limit for data upload

--- app.R
+++ app.R
@@ -7,7 +7,7 @@ library(shinyjs)
 library(markdown)
 library(shinyMatrix)
 
-options(shiny.maxRequestSize=100*1024^2) 
+options(shiny.maxRequestSize=100*2048^2) 
 fda_list <- c("fda10ab","fda10ad")
 m1 <- as.matrix(read_csv('docs/cas.csv')[42:66,2:4]) #%>% filter(TYPE_DISTURBANCE %in% x1))
 m2 <- as.matrix(read_csv('docs/cas.csv')[1:41,2:4]) #%>% filter(TYPE_DISTURBANCE %in% x2))
